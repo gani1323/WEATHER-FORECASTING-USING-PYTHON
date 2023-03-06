60e71da0d1a8a5ef5736e3f0614decaeccae24b4
Sanamsetty Ganesh <100744065+gani1323@users.noreply.github.com>
2023-03-06 19:53:36 +0530

    Delete WHEATHER FORECAST.py

--- WHEATHER FORECAST.py
+++
@@ -1,7 +0,0 @@
-import requests
-city = input('input the city name: ')
-print(city)
-print('Displaying Weather report for: ' + city)
-url = 'https://wttr.in/{}'.format(city)
-res = requests.get(url)
-print(res.text)
