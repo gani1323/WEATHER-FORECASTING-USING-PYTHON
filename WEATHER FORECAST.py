dbeef2fafa7ea771c81acc65f6977b9073861b8a
Sanamsetty Ganesh <100744065+gani1323@users.noreply.github.com>
2023-03-06 19:53:55 +0530

    Add files via upload

---
+++ WEATHER FORECAST.py
@@ -0,0 +1,7 @@
+import requests
+city = input('input the city name: ')
+print(city)
+print('Displaying Weather report for: ' + city)
+url = 'https://wttr.in/{}'.format(city)
+res = requests.get(url)
+print(res.text)
